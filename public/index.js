ff1fd50b0b1ea38deea08b27d541fce96ffaeeeb
Chris Kang <chriskangfamily@gmail.com>
2024-03-05 15:41:15 -0800

    part 1 done

--- public/index.js
+++ public/index.js
@@ -1,1 +1,28 @@
-// Your code here
+window.onload = async () => {
+// Header
+const h1Ele = document.createElement('h1')
+h1Ele.innerText = 'Kitten Pic'
+document.body.append(h1Ele)
+
+//API Call to Kitten Picture and creating the div element
+
+try {
+  const res = await fetch('https://api.thecatapi.com/v1/images/search')
+  const data = await res.json();
+
+  const link = data[0].url
+
+  const newCat = document.createElement('div')
+  const newImage = document.createElement('img')
+
+  newImage.setAttribute('class', 'image-itself')
+  newImage.src = link
+  newCat.appendChild(newImage)
+  newCat.setAttribute('class', 'new-cat-container')
+
+  document.body.append(newCat)
+
+} catch (e) {
+  window.alert("Couldn't fetch cat :(")
+}
+}
